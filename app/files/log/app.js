665565b183ab7cd2b6d78d6d92eb63761964251f
Shaghayegh Cloudtek <cloudtek.shaghayegh@gmail.com>
2021-03-12 10:13:11 +0330

    Main Files Added

---
+++ app/files/log/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {spawn} = require('child_process');
+const app = express();
+const ip = "0.0.0.0";
+const port = 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.get('/log', (req, res) => {
+    var startDate = req.query.startDate
+    var endDate = req.query.endDate
+
+    var result;
+    const python = spawn('python', ['log.py', startDate, endDate]);
+    // collect data from script
+    python.stdout.on('data', function (data) {
+        console.log('Pipe data from python script ...');
+        result = data.toString();
+    });
+    // in close event we are sure that stream from child process is closed
+    python.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code}`);
+        // send data to browser
+        res.send(result)
+    });
+
+})
+
+app.all("*", (req, res) => {
+    return res.status(404).json({
+        success: false,
+        msg: "404 Error - Not Found"
+    });
+});
+
+
+app.listen(port, ip, () => {
+    console.log(`Server is running on ${ip}:${port}`)
+})
